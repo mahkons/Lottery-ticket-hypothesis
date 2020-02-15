8ed22eb3d23b28e816397645dd310ef0f6261788
Makhnev Konstantin <mah.kons@gmail.com>
2020-02-16 01:01:50 +0300

    refactor train method

--- rl/train.py
+++ rl/train.py
@@ -1,5 +1,6 @@
 import torch
 from tqdm import tqdm
+import argparse
 
 from agent.Agent import Agent
 from agent.memory.ReplayMemory import ReplayMemory
@@ -12,33 +13,42 @@ from envs.LunarLander import LunarLander
 
 from params import CartPoleConfig, LunarLanderConfig
 
-device = torch.device("cuda")
 
-env = LunarLander()
-config = LunarLanderConfig()
+def train(epochs, prune_iters, device=torch.device('cpu')):
+    env = LunarLander()
+    config = LunarLanderConfig()
 
-memory = ReplayMemory(config.memory_config.memory_size)
-controller = ControllerDQN(env, memory, config, device=device)
-agent = Agent(env, controller, device=device)
+    memory = ReplayMemory(config.memory_config.memory_size)
+    controller = ControllerDQN(env, memory, config, device=device)
+    agent = Agent(env, controller, device=device)
 
-for iter in range(15):
-    plot_data = list()
-    epochs = 1000
-    pbar = tqdm(range(epochs))
-    for epoch in pbar:
-        reward, steps = agent.rollout(show=False)
-        pbar.set_description("Epoch [{}/{}]".format(epoch + 1, epochs))
-        pbar.write("Reward: {:.3f}".format(reward))
-        plot_data.append(reward)
-        if controller.optimization_completed():
-            break
+    for iter in tqdm(range(prune_iters)):
+        plot_data = list()
+        pbar = tqdm(range(epochs))
+        for epoch in pbar:
+            reward, steps = agent.rollout(show=False)
+            pbar.set_description("Epoch [{}/{}]".format(epoch + 1, epochs))
+            pbar.write("Reward: {:.3f}".format(reward))
+            plot_data.append(reward)
+            if controller.optimization_completed():
+                break
 
-    show_reward_plot(plot_data)
-    torch.save(plot_data, "plots/LunarLander_iter" + str(iter) + "_prune" +
-               str(0.8 ** iter))
+        show_reward_plot(plot_data)
+        torch.save(plot_data, "plots/LunarLander_iter" + str(iter) + "_prune" +
+                str(0.8 ** iter))
 
-    controller.prune(20)
-    controller.reinit()
-
+        controller.prune(20)
+        controller.reinit()
 
 
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', type=int, default=1000, required=False)
+    parser.add_argument('--prune-iters', type=int, default=1, required=False)
+    parser.add_argument('--device', type=str, default='cpu', required=False)
+    return parser
+
+
+if __name__ == "__main__":
+   args = create_parser().parse_args() 
+   train(args.epochs, args.prune_iters, torch.device(args.device))
