c146ef0720fbd1b7e563513ba92d0b3f1c8a8085
Makhnev Konstantin <mah.kons@gmail.com>
2020-03-15 23:43:34 +0300

    launch experiments in parallel!

---
+++ rl/launch_parallel.py
@@ -0,0 +1,60 @@
+import torch
+from multiprocessing import Pool
+from functools import partial
+
+from train import start_experiment
+from configs import Experiment
+from envs import LunarLander
+from params import LunarLanderConfig
+from pruners import RewindWrapper, ERPruner, LayerwisePruner, GlobalPruner
+from agent.stop_criterions import MaskDiffStop, EarlyBirdStop, NoStop
+
+device = torch.device('cpu')
+exp_list = list()
+
+def make_pruner__(rewind_epoch, pruner_constructor, net):
+    return RewindWrapper(pruner_constructor(net, device), rewind_epoch)
+
+def make_pruner(rewind_epoch, pruner_constructor):
+    return partial(make_pruner__, rewind_epoch, pruner_constructor)
+
+
+RANDOM_SEED=2020
+exp_list.append(Experiment(
+        opt_steps = 1000,
+        episodes = 10**10,
+        prune_iters = 2,
+        prune_percent = 20,
+        device = device,
+        logname = "E1_test_v3",
+        random_seed = RANDOM_SEED,
+        env = LunarLander(random_state=RANDOM_SEED),
+        hyperparams = LunarLanderConfig(),
+        stop_criterion = MaskDiffStop(eps=0),
+        pruner = make_pruner(0, ERPruner),
+    )
+)
+
+RANDOM_SEED=2021
+exp_list.append(Experiment(
+        opt_steps = 1000,
+        episodes = 10**10,
+        prune_iters = 2,
+        prune_percent = 20,
+        device = device,
+        logname = "E2_test_v3",
+        random_seed = RANDOM_SEED,
+        env = LunarLander(random_state=RANDOM_SEED),
+        hyperparams = LunarLanderConfig(),
+        stop_criterion = MaskDiffStop(eps=0),
+        pruner = make_pruner(0, ERPruner),
+    )
+)
+
+
+def launch_experiments():
+    with Pool(processes=4) as pool:
+        pool.map(start_experiment, exp_list)
+
+if __name__ == "__main__":
+    launch_experiments()
